0e8ede9b7b7cf5efa2f00e58513bed73600caff0
jasonKim <kimyk96@gmail.com>
2022-09-26 15:45:51 +0900

    서현아

--- src/main/java/com/hts/market/domain/board/Entity/BrdEntitiy.java
+++ src/main/java/com/hts/market/domain/board/Entity/BrdEntitiy.java
@@ -3,6 +3,7 @@ package com.hts.market.domain.board.Entity;
 import java.time.LocalDateTime;
 
 public class BrdEntitiy {
+    // 서현아 변수는 소문자로 시작해
     private Long BrdNo;
     private String BrdTitle;
     private String BrdContent;
