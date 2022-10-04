bbfd53690d8303c6a9f7fb2ff1155cf82b8975d7
juho9500 <pepsico9500gmail.com>
2022-10-05 08:41:17 +0900

    김주호 1차 mapper작성 완료

--- src/main/java/com/hts/market/domain/transaction/api/ReviewApi.java
+++ src/main/java/com/hts/market/domain/transaction/api/ReviewApi.java
@@ -16,25 +16,25 @@ public class ReviewApi {
     @Autowired ReviewApp reviewApp;
     @PostMapping("save")
     public ResponseEntity<Integer> save(Long reviewNo, Principal principal) {
-//        Integer result=ReviewApp.save();
+        Integer result=reviewApp.save();
         return ResponseEntity.ok().body(1);
     }
     @PutMapping("update")
     public ResponseEntity<Integer> update(Long reviewNo, Principal principal) {
-//        Integer result=ReviewApp.update();
+        Integer result=reviewApp.update();
         return ResponseEntity.ok().body(1);
     }
 
     @DeleteMapping("delete")
     public ResponseEntity<Integer> delete(Long reviewNo, Principal principal) {
-//        Integer result=ReviewApp.delete();
+        Integer result=reviewApp.delete();
         return ResponseEntity.ok().body(1);
     }
 
 
-    @GetMapping("/find-by-review-no")
+    @GetMapping("find-by-review-no")
     public ResponseEntity<TxDto.Read> findByReviewNo(ReviewDto.Read dto, Principal principal){
-//        TxDto.Read result = ReviewApp.findByReviewNo();
+        TxDto.Read result = reviewApp.findByReviewNo();
         return ResponseEntity.ok().body(null);
     }
 
