91335bc83098e6cea139c29bf78255b1093774ca
jasonKim <kimyk96@gmail.com>
2022-10-13 20:34:20 +0900

    member test 주석

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -12,31 +12,5 @@ $(function(){
     $("#nav_chat").click(function(){ $(location).attr("href", "/chat"); });
     // 회원
     $("#nav_member").click(function(){ $(location).attr("href", "/member"); });
-
-
-
-
 })
-
-// 회원 추가 정보 기입
-function signup(){
-    let memUsername = $("#phone").val();
-    let memNickname = $("#nickname").val();
-    let memEmail = $("#email").val();
-
-    $.ajax({
-        method: "PATCH",
-        url: "/api/v1/mem/update-by-mem-username",
-        data: { memUsername: memUsername, memNickname: memNickname, memEmail: memEmail }
-    })
-    .done(function(res){
-        $(location).attr("href", "/product");
-    })
-    .fail(function(res){
-        Swal.fire({
-            icon: 'error',
-            text: '정보 등록에 실패하였습니다.'
-        })
-    })
-}
 
