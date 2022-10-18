57c8cae90305737ebc68b7491b65517f308c23b1
고을호 <geh4019@gmail.com>
2022-10-18 10:31:56 +0900

    chat repo test 완

--- src/test/java/com/hts/market/domain/chat/repo/ChatRepoTest.java
+++ src/test/java/com/hts/market/domain/chat/repo/ChatRepoTest.java
@@ -1,6 +1,7 @@
 package com.hts.market.domain.chat.repo;
 
 import com.hts.market.domain.chat.dto.ChatDto;
+import com.hts.market.domain.member.dto.AddressDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -34,7 +35,7 @@ public class ChatRepoTest {
 
 
     @DisplayName("1)채팅저장")
-    @Test
+   // @Test
     public void save() {
 
         //given
@@ -48,7 +49,7 @@ public class ChatRepoTest {
         Assertions.assertThat(result).isEqualTo(1);
     }
     @DisplayName("2)채팅리스트&최근메세지")
-    @Test
+   // @Test
     public void findAllByChatMemNo() {
         //GIVEN
         Long chatMemNo = 1L;
@@ -62,7 +63,7 @@ public class ChatRepoTest {
     }
 
     @DisplayName("3)회원간 채팅내역")
-    @Test
+   // @Test
     public void findAllByChatMemNoAndChatPdtNo() {
         //GIVEN
         ChatDto.ListStartEnd listStartEnd = ChatDto.ListStartEnd.builder().chatMemNo(1L).chatPdtNo(1L).start(1).end(20).build();
@@ -76,14 +77,28 @@ public class ChatRepoTest {
     }
 
     @DisplayName("4)회원 관심")
-    @Test
+    //@Test
     public void countChatByPdtNoAndMemNo (){
         //GIVEN
-
+        ChatDto.ChatLike chatLike = ChatDto.ChatLike.builder().chatMemNo(2L).chatPdtNo(1L).build();
         //WHEN
-
+        Boolean result = chatrepo.countChatByPdtNoAndMemNo(chatLike);
         //THEN
+        Assertions.assertThat(result).isEqualTo(false);
+
     }
+
+    @DisplayName("5)상품별 메세지 카운트")
+    @Test
+    public void countChatByPdtNo(){
+        //GIVEN
+        Long dto = 1L;
+        //WHEN
+        Integer result = chatrepo.countChatByPdtNo(dto);
+        //THEN
+        Assertions.assertThat(result).isEqualTo(1);
+    }
+
 }
 
 
