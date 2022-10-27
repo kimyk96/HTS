48924573295a97bf2db6f750a147d50af7be31af
jasonKim <kimyk96@gmail.com>
2022-10-27 09:56:33 +0900

    배포 (채팅구현)

--- src/main/java/com/hts/market/global/controller/chat/WebSocketController.java
+++ src/main/java/com/hts/market/global/controller/chat/WebSocketController.java
@@ -6,25 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
-@RequestMapping("chat")
-@Secured("ROLE_USER")
-public class ChatController {
-    // 채팅 목록
-    @GetMapping("")
-    public String chat() {
-        return "chat/chat";
-    }
+public class WebSocketController {
+    @Autowired private ChatApp chatApp;
+    @Autowired private SimpMessagingTemplate simpMessagingTemplate;
 
-    // 채팅 방
-    @GetMapping("room")
-    public String chatroom() {
-        return "chat/chatroom";
+    @MessageMapping("/chat/{chatPdtNo}/{chatMemNo}")
+    public void simple(Message message, @DestinationVariable("chatPdtNo") String chatPdtNo, @DestinationVariable("chatMemNo") String chatMemNo) {
+        simpMessagingTemplate.convertAndSend("/topic/" + chatPdtNo + "/" + chatMemNo, chatApp.send(message, chatPdtNo, chatMemNo));
     }
 }
-
